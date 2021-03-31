b503c5e1e7c2a11f77b16e723082a75de074f76b
Andrew <g00398249@gmit.ie>
2021-03-31 21:50:44 +0100

    adding basic summary statistics

--- analysis.py
+++ analysis.py
@@ -7,9 +7,13 @@ import pandas as pd
 
 # Loading in Fisher’s Iris data set from a .data file
 # As file did not include column names, they were added using 'names' as an argument
-df = pd.read_csv('iris.data', names = ["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm", "class"])
-print(df)
+iris = pd.read_csv('iris.data', names = ["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm", "class"])
 
+
+# Overall summary 
+print(iris.describe())
+
+# Summaries seperated by class
 # Summary of sepal length
 
 
