99468c2d970d025f638d383a0586dfeb73252577
Andrew <g00398249@gmit.ie>
2021-04-15 15:44:17 +0100

    Summary stats remade, csv files created

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ As iris data set was in .data format, I first had to look up whether reading tha
 
 # Summary of sepal length histogram
 The curve shows that in general, setosa has the smallest sepal length, followed by versicolor, and finally virginica with the largest sepal length. However, there is significant overlap between species. The most notable takeaway is that 20 of the 50 setosa sepal length fall within 4.9mm and 5.1mm. 
+
+# pairplot
+
+I prefer to have grids on my plot to be able to better read where a point on the plot falls on a particular axis, and plt.grid() worked with the histograms. Howver, it didn't seem to work when I tried to use it to add a grid to the pairplot. Instead, I found that a way to at a grid to a pairplot is by using sns.set_style("whitegrid") before making the pairplot.
--- analysis.py
+++ analysis.py
@@ -10,32 +10,55 @@ import seaborn as sns
 # As file did not include column names, they were added using 'names' as an argument
 iris = pd.read_csv('iris.data', names = ["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm", "class"])
 
+# Add check to see if data is complete, no missing values etc
 
-# Overall summary 
-# Info to add dataframe to txt file found at: https://stackoverflow.com/questions/31247198/python-pandas-write-content-of-dataframe-into-text-file
-# Seperator set to comma so txt file data can easily be added to an excel sheet as a csv file if needed 
+
+# Overall summary
+# Checks for a text file by the specified name, and if it does not exist that file is created. It is in write mode meaning anything written to the 
+# file will be 
+with open("iris_summary.txt", "w") as f:
+    # Creates a pandas dataframe containing basic descriptive statistics from the dataset. This is then converted to a string so it can be 
+    # written to the text file. Two new lines are also specidied to seperate this output from subsequent outputs
+    descriptives = iris.describe()
+    f.write(descriptives.to_string() + "\n\n") 
+    
+    sepal_length_summary = iris[["sepal length in cm", "class"]].groupby("class").describe()
+    f.write(sepal_length_summary.to_string() + "\n\n")
+
+    sepal_width_summary = iris[["sepal width in cm", "class"]].groupby("class").describe()
+    f.write(sepal_width_summary.to_string() + "\n\n")
+
+    petal_length_summary = iris[["petal length in cm", "class"]].groupby("class").describe()
+    f.write(petal_length_summary.to_string() + "\n\n")
+
+    petal_width_summary = iris[["petal width in cm", "class"]].groupby("class").describe()
+    f.write(petal_width_summary.to_string() + "\n\n")
+
+
+# Also decided to output the summaries to csv files as these can be easier to work with should someone want to do further analysis using 
+# the output summaries
+# Info to add dataframe to csv file found at: https://stackoverflow.com/questions/31247198/python-pandas-write-content-of-dataframe-into-text-file
+# Seperator set to comma so csv file data can easily be added to an excel sheet if needed 
 # "r" before the filename means the file path is read literally by python, to avoid conflict with any characters 
 # that may have other purposes in python https://discuss.python.org/t/what-does-r-define-in-file-path/3646
-# First dataframe to txt file output set to write, while rest are set to append. This is to avoid tables being repeated each time 
-# this code is run
+# Dataframe to csv file output set to write so it will overwrite anything existing on the csv file each time code is run.
 iris.describe()
-iris.describe().to_csv(r'iris_summary.txt', sep=',', mode='w')
+iris.describe().to_csv(r'iris_summary_descriptive.csv', sep=',', mode='w')
 
-"""
 # Summaries seperated by class
 # Summary of sepal length
-iris[["sepal length in cm", "class"]].groupby("class").describe().to_csv(r'iris_summary.txt', index="class", sep=',', mode='a')
+iris[["sepal length in cm", "class"]].groupby("class").describe().to_csv(r'sepal_length_summary.csv', index="class", sep=',', mode='w')
 
 # Summary of sepal width
-iris[["sepal width in cm", "class"]].groupby("class").describe().to_csv(r'iris_summary.txt', index="class", sep=',', mode='a')
+iris[["sepal width in cm", "class"]].groupby("class").describe().to_csv(r'sepal_width_summary.csv', index="class", sep=',', mode='w')
 
 # Summary of petal length
-iris[["petal length in cm", "class"]].groupby("class").describe().to_csv(r'iris_summary.txt', index="class", sep=',', mode='a')
+iris[["petal length in cm", "class"]].groupby("class").describe().to_csv(r'petal_length_summary.csv', index="class", sep=',', mode='w')
 
 # Summary of petal width
-iris[["petal width in cm", "class"]].groupby("class").describe().to_csv(r'iris_summary.txt', index="class", sep=',', mode='a')
+iris[["petal width in cm", "class"]].groupby("class").describe().to_csv(r'petal_width_summary.csv', index="class", sep=',', mode='w')
 
-
+"""
 # Histogram of of sepal length
 sns.histplot(iris, x = "sepal length in cm", hue="class", kde=True, binwidth=0.2)
 plt.grid()
@@ -44,7 +67,7 @@ plt.savefig('HistogramSepalLength.png')
 plt.clf() # Clears the plot so a new plot can be created. Without this, the subsequent plots are combined into the previous plot
 
 # Histogram of sepal width
-sns.histplot(iris, x = "sepal width in cm", hue="class", kde=True, binwidth=0.2)
+sns.histplot(iris, x = "sepal width in cm", hue="class", kde=True, binwidth=0.2,)
 plt.grid()
 plt.savefig('HistogramSepalWidth.png')
 plt.clf()
@@ -60,5 +83,22 @@ sns.histplot(iris, x = "petal width in cm", hue="class", kde=True, binwidth=0.2)
 plt.grid()
 plt.savefig('HistogramPetalWidth.png')
 plt.clf()
-"""
+
+
 # Scatterplots
+# Using different markers as well as colour to make the different classes more clear, which is important ahould someone with colourblindness find 
+# it tricky to differentiate classes by colour alone.
+sns.set_style("whitegrid")
+sns.pairplot(iris, hue="class", markers=["o", "s", "D"])
+plt.savefig('Pairplot.png')
+plt.clf()
+
+
+# Density function
+
+
+# Boxplot
+
+
+# Violin plot
+"""
